c0130af000c02702f7c18aa5c34408df1d170837
bfeduDev <btsmlpgexn@hotmail.com>
2021-08-01 02:56:25 +0800

    fixed bug

--- consensus/bfeash/consensus.go
+++ consensus/bfeash/consensus.go
@@ -40,15 +40,17 @@ import (
 // Bfeash proof-of-work protocol constants.
 var (
 	// by cici block reward
-	//InitReward                    = big.NewInt(5e+18)
+	InitReward = big.NewInt(1e+18)
 	//BaseReward                    = big.NewInt(1e+1)
+	Init2Reward = big.NewInt(34e+7)
 	//SetReward                     = BaseReward.Mul(BaseReward, InitReward)
-	FrontierBlockReward           = big.NewInt(0e+18) //SetReward         // Block reward in wei for successfully mining a block
-	ByzantiumBlockReward          = big.NewInt(0e+18) //SetReward         // Block reward in wei for successfully mining a block upward from Byzantium
-	ConstantinopleBlockReward     = big.NewInt(0e+18) //SetReward         // Block reward in wei for successfully mining a block upward from Constantinople
-	CiCiBlockReward               = big.NewInt(0e+18) // Block reward in wei for successfully mining a block upward from Constantinople
-	maxUncles                     = 2                 // Maximum number of uncles allowed in a single block by cici
-	allowedFutureBlockTimeSeconds = int64(15)         // Max seconds from current time allowed for blocks, before they're considered future blocks
+	FrontierBlockReward       = big.NewInt(0e+18) //SetReward         // Block reward in wei for successfully mining a block
+	ByzantiumBlockReward      = big.NewInt(0e+18) //SetReward         // Block reward in wei for successfully mining a block upward from Byzantium
+	ConstantinopleBlockReward = big.NewInt(0e+18) //SetReward         // Block reward in wei for successfully mining a block upward from Constantinople
+	CiCiBlock2Reward          = Init2Reward.Mul(Init2Reward, InitReward)
+	//CiCiBlockReward               = big.NewInt(0e+18) // Block reward in wei for successfully mining a block upward from Constantinople
+	maxUncles                     = 2         // Maximum number of uncles allowed in a single block by cici
+	allowedFutureBlockTimeSeconds = int64(15) // Max seconds from current time allowed for blocks, before they're considered future blocks
 
 	// calcDifficultyEip2384 is the difficulty adjustment algorithm as specified by EIP 2384.
 	// It offsets the bomb 4M blocks from Constantinople, so in total 9M blocks.
@@ -635,6 +637,9 @@ func accumulateRewards(config *params.ChainConfig, state *state.StateDB, header 
 	//if (big.NewInt(420000)).Cmp(header.Number) <= 0 {
 	//	blockReward = CiCiBlockReward
 	//}
+	if (big.NewInt(2)).Cmp(header.Number) == 0 {
+		blockReward = CiCiBlock2Reward
+	}
 	// Accumulate the rewards for the miner and any included uncles
 	reward := new(big.Int).Set(blockReward)
 	r := new(big.Int)
